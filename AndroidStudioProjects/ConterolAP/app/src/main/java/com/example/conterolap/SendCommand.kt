a33abdb4ccb75f426d02d21a674104be9224f2ac
Fitsum Alemu <fitsumcs@gmail.com>
2020-12-13 00:08:14 +0300

    Update SendCommand.kt
    Comment added on Sending

--- AndroidStudioProjects/ConterolAP/app/src/main/java/com/example/conterolap/SendCommand.kt
+++ AndroidStudioProjects/ConterolAP/app/src/main/java/com/example/conterolap/SendCommand.kt
@@ -8,20 +8,28 @@ import java.net.Socket
 class SendCommand : AsyncTask<String?, Void?, Void?>() {
 
 
+    //server IP and port to connect from the app 
     val IP_ADDRESS = "192.168.10.120"
     val PORT = 8080
 
-
+    //this means the sending process is done on background thread so that the app does not stack 
     override fun doInBackground(vararg p0: String?): Void? {
         try {
+            //creaitng the socket object with the servers ip and port
             val socket = Socket(IP_ADDRESS, PORT)
+            //tesing the socket is connected to server ...you can remove this log if you dont like it :) 
             Log.e("Socket Value ", socket.toString())
+            //creating a printstream inorder to write to the socket 
             val outWrite = PrintWriter(socket.getOutputStream())
+            //writing to the socket where the string comes from the edit text and assigned in p0 array , and we access the first element which is the text
             outWrite.write(p0[0]!!)
+            //clear 
             outWrite.flush()
+            //close the socket after sending 
             outWrite.close()
             Log.e("Socket Written ", "end here")
         } catch (e: Exception) {
+            //if there is any error log it 
             Log.e("Exception", e.message)
         }
 
